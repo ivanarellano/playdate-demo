a36bfab6954eb1924a4a3ff3fe287c6fdd139382
Ivan <ia@ivanarellano.com>
2022-05-02 23:08:05 -0400

    Add timer

--- source/main.lua
+++ source/main.lua
@@ -5,11 +5,20 @@ import "CoreLibs/timer"
 
 local gfx <const> = playdate.graphics
 
-local playerSprite = nil
-
+local playTimer = nil
+local playTime = 30 * 1000
 local playerSpeed = 3
 
+local playerSprite = nil
+
+local function resetTimer()
+	playTimer = playdate.timer.new(
+		playTime, playTime, 0, playdate.easingFunctions.linear)
+end
+
 local function initialize()
+	resetTimer()
+	
 	local playerImage = gfx.image.new("images/player")
 	playerSprite = gfx.sprite.new(playerImage)
 	playerSprite:moveTo(200,120)
@@ -28,20 +37,30 @@ end
 initialize()
 
 function playdate.update()
-	if playdate.buttonIsPressed(playdate.kButtonUp) then
-		playerSprite:moveBy(0, -playerSpeed)
+	if playTimer.value == 0 then
+		if playdate.buttonJustPressed(playdate.kButtonA) then
+			resetTimer()
+		end
+	else
+		if playdate.buttonIsPressed(playdate.kButtonUp) then
+			playerSprite:moveBy(0, -playerSpeed)
+		end
+		if playdate.buttonIsPressed(playdate.kButtonRight) then
+			playerSprite:moveBy(playerSpeed, 0)
+		end
+		if playdate.buttonIsPressed(playdate.kButtonDown) then
+			playerSprite:moveBy(0, playerSpeed)
+		end
+		if playdate.buttonIsPressed(playdate.kButtonLeft) then
+			playerSprite:moveBy(-playerSpeed, 0)
+		end
+		if playdate.buttonIsPressed(playdate.kButtonUp) then
+			playerSprite:moveBy(0, -playerSpeed)
+		end
 	end
-	if playdate.buttonIsPressed(playdate.kButtonRight) then
-		playerSprite:moveBy(playerSpeed, 0)
-	end
-	if playdate.buttonIsPressed(playdate.kButtonDown) then
-		playerSprite:moveBy(0, playerSpeed)
-	end
-	if playdate.buttonIsPressed(playdate.kButtonLeft) then
-		playerSprite:moveBy(-playerSpeed, 0)
-	end
-	if playdate.buttonIsPressed(playdate.kButtonUp) then
-		playerSprite:moveBy(0, -playerSpeed)
-	end
+
+	playdate.timer.updateTimers()
 	gfx.sprite.update()
+
+	gfx.drawText("Time: " .. math.ceil(playTimer.value/1000), 5, 5)
 end
