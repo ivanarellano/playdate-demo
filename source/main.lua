c933d4b17e9feb833040b29023ab39202cc6e54c
Ivan <ia@ivanarellano.com>
2022-05-02 23:02:10 -0400

    Add background and player input

--- source/main.lua
+++ source/main.lua
@@ -7,15 +7,41 @@ local gfx <const> = playdate.graphics
 
 local playerSprite = nil
 
+local playerSpeed = 3
+
 local function initialize()
 	local playerImage = gfx.image.new("images/player")
 	playerSprite = gfx.sprite.new(playerImage)
 	playerSprite:moveTo(200,120)
 	playerSprite:add()
+
+	local bgImage = gfx.image.new("images/background")
+	gfx.sprite.setBackgroundDrawingCallback(
+		function (x, y, width, height)
+			gfx.setClipRect(x, y, width, height)
+			bgImage:draw(0,0)
+			gfx.clearClipRect()
+		end
+	)
 end
 
 initialize()
 
 function playdate.update()
+	if playdate.buttonIsPressed(playdate.kButtonUp) then
+		playerSprite:moveBy(0, -playerSpeed)
+	end
+	if playdate.buttonIsPressed(playdate.kButtonRight) then
+		playerSprite:moveBy(playerSpeed, 0)
+	end
+	if playdate.buttonIsPressed(playdate.kButtonDown) then
+		playerSprite:moveBy(0, playerSpeed)
+	end
+	if playdate.buttonIsPressed(playdate.kButtonLeft) then
+		playerSprite:moveBy(-playerSpeed, 0)
+	end
+	if playdate.buttonIsPressed(playdate.kButtonUp) then
+		playerSprite:moveBy(0, -playerSpeed)
+	end
 	gfx.sprite.update()
 end
