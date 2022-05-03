71fa5f8a1f177f49e44d399d3e58319a1022114e
Ivan <ia@ivanarellano.com>
2022-05-02 23:20:48 -0400

    Add score and coin collision

--- source/main.lua
+++ source/main.lua
@@ -10,19 +10,35 @@ local playTime = 30 * 1000
 local playerSpeed = 3
 
 local playerSprite = nil
+local coinSprite = nil
+local score = 0
 
 local function resetTimer()
 	playTimer = playdate.timer.new(
 		playTime, playTime, 0, playdate.easingFunctions.linear)
 end
 
+local function moveCoin()
+	local randX = math.random(40, 360)
+	local randY = math.random(40, 200)
+	coinSprite:moveTo(randX, randY)
+end
+
 local function initialize()
 	resetTimer()
-	
+	math.randomseed(playdate.getSecondsSinceEpoch())
+
 	local playerImage = gfx.image.new("images/player")
 	playerSprite = gfx.sprite.new(playerImage)
 	playerSprite:moveTo(200,120)
+	playerSprite:setCollideRect(0, 0, playerSprite:getSize())
 	playerSprite:add()
+
+	local coinImage = gfx.image.new("images/coin")
+	coinSprite = gfx.sprite.new(coinImage)
+	moveCoin()
+	coinSprite:setCollideRect(0, 0, coinSprite:getSize())
+	coinSprite:add()
 
 	local bgImage = gfx.image.new("images/background")
 	gfx.sprite.setBackgroundDrawingCallback(
@@ -40,6 +56,8 @@ function playdate.update()
 	if playTimer.value == 0 then
 		if playdate.buttonJustPressed(playdate.kButtonA) then
 			resetTimer()
+			moveCoin()
+			score = 0
 		end
 	else
 		if playdate.buttonIsPressed(playdate.kButtonUp) then
@@ -57,10 +75,17 @@ function playdate.update()
 		if playdate.buttonIsPressed(playdate.kButtonUp) then
 			playerSprite:moveBy(0, -playerSpeed)
 		end
+
+		local collisions = coinSprite:overlappingSprites()
+		if #collisions >= 1 then
+			moveCoin()
+			score += 1
+		end
 	end
 
 	playdate.timer.updateTimers()
 	gfx.sprite.update()
 
 	gfx.drawText("Time: " .. math.ceil(playTimer.value/1000), 5, 5)
+	gfx.drawText("Score: " .. score, 320, 5)
 end
